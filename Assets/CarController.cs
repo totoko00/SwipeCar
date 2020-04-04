a3b3494d35d03b22185b54c92658e843dd7d09f3
totoko <totoko.staphi@gmail.com>
2020-04-04 15:46:24 +0900

    スワイプで車を走らせてぎりぎりで止めるゲーム

---
+++ Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))        //マウスクリック
+        {
+            //マウスクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            //スワイプの長さを初速度に変換する
+            this.speed = swipeLength / 1000.0f;
+
+            //効果音を鳴らす
+            GetComponent<AudioSource>().Play();
+        }
+
+        transform.Translate(this.speed, 0, 0);  //移動
+        this.speed *= 0.98f;                    //減速
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car"); //Findメソッド「オブジェクト名を引数に取り、引数名と同じものがゲームシーン中にあれば、そのオブジェクトを返す」
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length >= 0)
+        {
+            this.distance.GetComponent<Text>().text = "ゴールまで" + length.ToString("F2") + "m";   
+        }
+        else
+        {
+            this.distance.GetComponent<Text>().text = "ゲームオーバー";
+        }
+    }
+}
